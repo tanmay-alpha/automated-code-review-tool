4e4f74f9473e0133b9554a54cce4a1004c7a7b4d
Tanmay Mangal <254906467+tanmay-alpha@users.noreply.github.com>
2026-08-01 01:17:06 +0530

    fix(test): configure PostgreSQL driver and quality score parity fixtures

--- apps/api/src/test/java/com/automatedcodereviewtool/migration/V6MlDatasetMigrationTest.java
+++ apps/api/src/test/java/com/automatedcodereviewtool/migration/V6MlDatasetMigrationTest.java
@@ -40,6 +40,8 @@ class V6MlDatasetMigrationTest {
         r.add("spring.datasource.url", PG::getJdbcUrl);
         r.add("spring.datasource.username", PG::getUsername);
         r.add("spring.datasource.password", PG::getPassword);
+        r.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+        r.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
         r.add("spring.flyway.enabled", () -> "true");
         r.add("spring.flyway.locations", () -> "classpath:db/migration");
         r.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
